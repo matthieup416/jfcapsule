61a98f8d7c01d4c3c26a1e7950d0374e261cca4a
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-02-23 08:19:54 +0100

    m

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,32 @@
-var express = require('express');
-var router = express.Router();
-var uniqid = require('uniqid');
-var fs = require('fs');
+var express = require('express')
+var router = express.Router()
+var uniqid = require('uniqid')
+var fs = require('fs')
 
-var cloudinary = require('cloudinary').v2;
-cloudinary.config({ 
-  cloud_name: 'dvqjak***', 
-  api_key: '767287626552***', 
-  api_secret: 'BRfbaQzy3xSWMq0dNqdLAS***' 
-});
+var cloudinary = require('cloudinary').v2
+cloudinary.config({
+  cloud_name: 'matthieudev',
+  api_key: '633726389759375',
+  api_secret: '5QYtAd670KXAfa9UADMIUPxsu2A',
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-router.post('/upload', async function(req, res, next) {
-  
-  var pictureName = './tmp/'+uniqid()+'.jpg';
-  var resultCopy = await req.files.avatar.mv(pictureName);
-  if(!resultCopy) {
-    var resultCloudinary = await cloudinary.uploader.upload(pictureName);
-    res.json(resultCloudinary);
+router.post('/upload', async function (req, res, next) {
+  var pictureName = './tmp/' + uniqid() + '.jpg'
+  var resultCopy = await req.files.avatar.mv(pictureName)
+  console.log('resultCopy : ', resultCopy)
+  if (!resultCopy) {
+    var resultCloudinary = await cloudinary.uploader.upload(pictureName)
+    res.json(resultCloudinary)
   } else {
-    res.json({error: resultCopy});
+    res.json({ error: resultCopy })
   }
 
-  fs.unlinkSync(pictureName);
-  
-});
+  fs.unlinkSync(pictureName)
+})
 
-module.exports = router;
+module.exports = router
